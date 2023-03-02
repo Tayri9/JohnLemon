026b47d4862e4756d076c6d2add670e80ca436c7
Tayri <tayrifernandez@gmail.com>
2023-03-02 09:01:34 +0000

    modificaiones

--- JohnLemon/Assets/Scripts/Observer.cs
+++ JohnLemon/Assets/Scripts/Observer.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Observer : MonoBehaviour
 {
     public GameEnding gameEnding;
     public Transform player;
+    public float time = 2f;
+    public GameObject textExclamacion;
 
+    AudioSource m_AudioSource;
     bool m_IsPlayerInRange;
+    float m_timer = 0;
+    float m_timerExclamacion = 0;
+    bool m_visto;
+
+    private void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -21,9 +33,36 @@ public class Observer : MonoBehaviour
             {
                 if (raycastHit.collider.transform == player)
                 {
-                    gameEnding.CaughtPlayer();
+                    m_visto = true;
+                    m_timer += Time.deltaTime;
+
+                    //textExclamacion.SetActive(true);
+                    if (!m_AudioSource.isPlaying)
+                    {
+                        m_AudioSource.Play();
+                    }
+
+                    if (m_timer >= time)
+                    {
+                        gameEnding.CaughtPlayer();
+                        m_timer = 0;
+                        //textExclamacion.SetActive(false);
+                    }                    
                 }
             }
+        }
+
+        if (m_visto)
+        {
+            textExclamacion.SetActive(true);
+            m_timerExclamacion += Time.deltaTime;
+            if(m_timerExclamacion >= time)
+            {
+                textExclamacion.SetActive(false);
+                m_visto = false;
+                m_timerExclamacion = 0;
+            }
+
         }
     }
 
@@ -40,6 +79,9 @@ public class Observer : MonoBehaviour
         if (other.transform == player)
         {
             m_IsPlayerInRange = false;
+            m_timer = 0;
+            m_AudioSource.Stop();
+            //textExclamacion.SetActive(false);
         }
     }
 }
